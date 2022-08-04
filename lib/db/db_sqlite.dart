148fd070277d471b68dd13a92a698856f528284a
parvez6084 <parvez.ka19@gmail.com>
2022-08-04 10:01:35 +0600

    project update and local add

--- lib/db/db_sqlite.dart
+++ lib/db/db_sqlite.dart
@@ -1,14 +1,8 @@
-import 'dart:io';
-
 import 'package:card_x/model/category_model.dart';
 import 'package:card_x/model/contact_model.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:get/get.dart';
 import 'package:path/path.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:encrypt/encrypt.dart' as encrypt;
-import 'dart:convert' as convert;
 
 class DBSQLite extends GetxService {
   DBSQLite._internal();
@@ -127,50 +121,5 @@ class DBSQLite extends GetxService {
         mapList.length, (index) => ContactModel.fromMap(mapList[index]));
   }*/
 
-  List<String> tables = [];
-  static const SECRET_KEY = "CardX_PRIVATE_KEY_ENCRYPT_2022";
 
-  Future<String> generateBackup({bool isEncrypted = true}) async {
-    print('GENERATE BACKUP');
-    var dbs = await dbSqLite.database;
-    List data = [];
-    List<Map<String, dynamic>> listMaps = [];
-
-    for (var i = 0; i < tables.length; i++) {
-      listMaps = await dbs.query(tables[i]);
-      data.add(listMaps);
-    }
-
-    List backups = [tables, data];
-    String json = convert.jsonEncode(backups);
-
-    if (isEncrypted) {
-      var key = encrypt.Key.fromUtf8(SECRET_KEY);
-      var iv = encrypt.IV.fromLength(16);
-      var encrypter = encrypt.Encrypter(encrypt.AES(key));
-      var encrypted = encrypter.encrypt(json, iv: iv);
-      return encrypted.base64;
-    } else {
-      return json;
-    }
-  }
-
-  Future<void> restoreBackup(String backup, {bool isEncrypted = true}) async {
-    var dbs = await dbSqLite.database;
-    Batch batch = dbs.batch();
-    var key = encrypt.Key.fromUtf8(SECRET_KEY);
-    var iv = encrypt.IV.fromLength(16);
-    var encrypter = encrypt.Encrypter(encrypt.AES(key));
-
-    List json = convert
-        .jsonDecode(isEncrypted ? encrypter.decrypt64(backup, iv: iv) : backup);
-
-    for (var i = 0; i < json[0].length; i++) {
-      for (var k = 0; k < json[1][i].length; k++) {
-        batch.insert(json[0][i], json[1][i][k]);
-      }
-    }
-    await batch.commit(continueOnError: false, noResult: true);
-    print('RESTORE BACKUP');
-  }
 }
